7d515481672661b3de3145737446ff8c7972fa8c
Amir Mofasser <amir.mofasser@volvocars.com>
2015-09-30 11:30:22 +0200

    Support listen on specific host

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require('./app/config/config'),
 	log = require('./app/config/log')();
 
 // Start the server
-app.listen(config.port);
+app.listen(config.port, config.host);
 
 // Set-up logging
 log.add(log.transports.File, config.log);
