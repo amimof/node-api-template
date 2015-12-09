52d60f731e16f744b4bead73d4cf65715c2130a2
Amir Mofasser <amir.mofasser@volvocars.com>
2015-12-09 17:37:30 +0100

    Server listens on 0.0.0.0 by default

--- app/config/config.js
+++ app/config/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-	host: process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+	host: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 	db: {
 		url: 'mongodb://ansible:ansible@localhost:27017/ansible'
 	},
